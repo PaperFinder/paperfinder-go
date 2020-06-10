354fff46a64a4ff4132b51d74075ab27ced26b4d
frogstair <daniladudkin412@gmail.com>
2020-06-10 20:37:18 +0300

    Added past papers and reader

---
+++ filereader/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/ledongthuc/pdf"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Not enough args")
+		return
+	}
+	dir := os.Args[1]
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if path == os.Args[1] {
+			return nil
+		}
+		strdata, err := readPdf(path) // Read local pdf file
+		if err != nil {
+			panic(err)
+		}
+
+		fullstopReg := regexp.MustCompile(`\.{2,}`)
+		markReg := regexp.MustCompile(`\[\d+\]`)
+		markRegRound := regexp.MustCompile(`\(\d+\)`)
+		doubleSpace := regexp.MustCompile(`\ {2,}`)
+
+		strdata = fullstopReg.ReplaceAllLiteralString(strdata, "")
+		strdata = markReg.ReplaceAllLiteralString(strdata, "")
+		strdata = markRegRound.ReplaceAllLiteralString(strdata, "")
+		strdata = doubleSpace.ReplaceAllLiteralString(strdata, " ")
+
+		strdata = strings.ReplaceAll(strdata, ".", " ")
+		strdata = strings.ReplaceAll(strdata, "_", "")
+		strdata = strings.ReplaceAll(strdata, "\r\n", " ")
+
+		err = ioutil.WriteFile(dir+"/"+info.Name()+".filtered", []byte(strdata), 0644)
+
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readPdf(path string) (string, error) {
+	f, r, err := pdf.Open(path)
+	// remember close file
+	defer f.Close()
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	b, err := r.GetPlainText()
+	if err != nil {
+		return "", err
+	}
+	buf.ReadFrom(b)
+	return buf.String(), nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module paperfinder-go
 go 1.14
 
 require (
+	code.sajari.com/docconv v1.1.0 // indirect
+	github.com/JalfResi/justext v0.0.0-20170829062021-c0282dea7198 // indirect
+	github.com/advancedlogic/GoOse v0.0.0-20191112112754-e742535969c1 // indirect
+	github.com/blevesearch/bleve v1.0.9 // indirect
+	github.com/blevesearch/cld2 v0.0.0-20200327141045-8b5f551d37f5 // indirect
+	github.com/cznic/b v0.0.0-20181122101859-a26611c4d92d // indirect
+	github.com/ikawaha/kagome.ipadic v1.1.2 // indirect
+	github.com/jmhodges/levigo v1.0.0 // indirect
 	github.com/kataras/iris/v12 v12.1.8
+	github.com/ledongthuc/pdf v0.0.0-20200323191019-23c5852adbd2
+	github.com/levigross/exp-html v0.0.0-20120902181939-8df60c69a8f5 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/tebeka/snowball v0.4.2 // indirect
+	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
 )
