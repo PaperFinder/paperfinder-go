58a41b4665dd1b4eebf8d477ee6bcb5d9e2b2607
Cents 02 <discord@yes.com>
2020-06-11 12:50:16 -0700

    Added paper info extraction + db

--- _filereader/main.go
+++ _filereader/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -16,7 +19,7 @@ func main() {
 		return
 	}
 	dir := os.Args[1]
-
+	fmt.Println(dir)
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if path == os.Args[1] {
 			return nil
@@ -38,16 +41,61 @@ func main() {
 		markReg := regexp.MustCompile(`\[\d+\]`)
 		markRegRound := regexp.MustCompile(`\(\d+\)`)
 		doubleSpace := regexp.MustCompile(`\ {2,}`)
+		unitrgx := regexp.MustCompile(`/(Unit: [1-9]+)/i`)
+		subjectrgx := regexp.MustCompile(`/( .+ Advanced)/i`)
+		yearrgx := regexp.MustCompile(`©\d+`) //Pearson is up to date with copyright so why not use it
 
 		strdata = strings.ReplaceAll(strdata, ".", " ")
 		strdata = strings.ReplaceAll(strdata, "_", "")
 		strdata = strings.ReplaceAll(strdata, "\n", " ")
 		strdata = strings.ReplaceAll(strdata, "\r", " ")
 
+		//To improve in the future
+		fmt.Println(string(unitrgx.Find([]byte(strdata))))
+		fmt.Println(string(subjectrgx.Find([]byte(strdata))))
+		fmt.Println(string(yearrgx.Find([]byte(strdata))))
+		unit := strings.Split(string(unitrgx.Find([]byte(strdata))), " ")[1]
+		subject := strings.Split(string(subjectrgx.Find([]byte(strdata))), " ")[1]
+		year := strings.Split(string(yearrgx.Find([]byte(strdata))), "©")[1]
+		month := ""
+		switch fmonth := newName[:1]; fmonth {
+		case "Ja":
+			month = "January"
+		case "Ju":
+			month = "June"
+		case "Oc":
+			month = "October"
+		default:
+			month = "NA"
+		}
+		papername := month + " " + year + " QP - Unit " + unit + " Edexcel " + subject + " A-level.pdf"
+		//Lets assume everything is from pmt for now
+		qpl := "https://pmt.physicsandmathstutor.com/download/" + subject + "/A-level/Past-Papers/Edexcel-IAL/Unit-" + unit + "/" + papername
+		msl := strings.ReplaceAll(qpl, "QP", "MS")
+		uunit, err := strconv.Atoi(unit)
+		if err != nil {
+			uunit = 0
+		}
+		db, _ := sql.Open("sqlite3", "../db/sqlite-database.db")
+		insertpaper := `INSERT INTO papers(ID, filename, subject,unit,qpl,msl) VALUES (,?,?,?,?,?)`
+
+		statement, err := db.Prepare(insertpaper)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		_, err = statement.Exec(newName, subject, uunit, qpl, msl)
+
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		db.Close()
 		strdata = fullstopReg.ReplaceAllLiteralString(strdata, "")
 		strdata = markReg.ReplaceAllLiteralString(strdata, "")
 		strdata = markRegRound.ReplaceAllLiteralString(strdata, "")
 		strdata = doubleSpace.ReplaceAllLiteralString(strdata, " ")
+
+		db.Close()
 
 		err = ioutil.WriteFile(newName, []byte(strdata), 0644)
 
