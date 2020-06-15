c0c8f5e9aec697cbc72b6bac0e04dba3b61d612f
Cents 02 <discord@yes.com>
2020-06-15 10:14:15 -0700

    Query the available subjects onload

--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,6 @@
 function search() {
     var s = document.getElementById("subjects");
+
     var subject = s.options[s.selectedIndex].value;
 
     var q = document.getElementById("question")
@@ -11,6 +12,34 @@ function search() {
             console.log(xhr.responseText)
         }
     };
-    xhr.open('GET', '/finder?q=' + question + '&s=' + subject);
+    xhr.open('GET', '/finder?q=' + question + '&s=' + subject,true);
+    xhr.onload  = function() {
+        var jsonResponse = req.response;
+        
+     };
     xhr.send()
+
+}
+function onload() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/subjects',true);
+    
+    xhr.onload  = function() {
+        var jsonResponse =  JSON.parse(xhr.responseText);
+        var subj = jsonResponse.Subjects.split(",")
+        subj.forEach(addcat)
+     };
+     xhr.send()
+    
+
+
+}
+function addcat(item,index) {
+    var s = document.getElementById("subjects");
+    var opt = document.createElement('option')
+    opt.value = item;
+    opt.innerHTML = item;
+    s.appendChild(opt);
+
+
 }
