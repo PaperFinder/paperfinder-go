e72e1bc3782e55798dd639865bda9e97afdbf8de
Cents 02 <discord@yes.com>
2020-09-08 14:16:53 -0700

    added a script until proper scrapping exists

---
+++ _filereader/sites.sh
@@ -0,0 +1,17 @@
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/a-level-geography/
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/a-level-economics/
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/a-level-biology/
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/a-level-physics/
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/a-level-psychology/
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/a-level-maths/
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/a-level-chemistry/
+
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-geography
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-history
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-Science
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-Maths
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-Economics
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-Chemistry
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-biology
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-english-literature/
+./_filereader crawl https://www.physicsandmathstutor.com/past-papers/gcse-english-language/
