ca64d13c84ba172dd23d241558b1cc71d35e2473
danielpancake <dsatarov@outlook.com>
2020-09-10 21:02:04 +0500

    Fix result vulnerability

--- web/js/index.js
+++ web/js/index.js
@@ -1,43 +1,50 @@
 function search() {
     onload();
+
     var s = document.getElementById("subjects");
 
     var subject = s.options[s.selectedIndex].value;
 
-    var q = document.getElementById("question")
-    var question = q.value
+    var q = document.getElementById("question");
+    var question = q.value;
 
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(response) {
+
+    xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             console.log(xhr.responseText)
         }
     };
+
     xhr.open('GET', '/finder?q=' + question + '&s=' + subject,false);
     
     xhr.send()
-        var jsonResponse =  JSON.parse(xhr.responseText);
-        document.getElementById("result").innerHTML = jsonResponse.Query
+        var jsonResponse = JSON.parse(xhr.responseText);
+        document.getElementById("result").innerHTML = gabi_content(jsonResponse.Query);
+
         if (jsonResponse.Found == 'True') {
-            
-            document.getElementById("result").innerText = "\"" +jsonResponse.Query + "\" was found in "+ jsonResponse.Paper
-            document.getElementById("qpl").href = jsonResponse.QPL
-            document.getElementById("msl").href = jsonResponse.MSL
+            document.getElementById("result").innerText = "\"" + gabi_content(jsonResponse.Query) + "\" was found in "+ jsonResponse.Paper;
+            document.getElementById("qpl").href = jsonResponse.QPL;
+            document.getElementById("msl").href = jsonResponse.MSL;
             document.getElementById("result").style.display = '';
             document.getElementById("qpl").style.display = '';
             document.getElementById("msl").style.display = '';
-
         } else {
-            document.getElementById("result").innerHTML = "\"" +jsonResponse.Query + "\"was found in no papers"
+            document.getElementById("result").innerHTML = "\"" + gabi_content(jsonResponse.Query) + "\"was found in no papers"
         }
  };
 
-function onload() {
+function gabi_content(content) {
+    var temp = document.createElement("div");
+    temp.innerHTML = content;
+    
+    return temp.textContent || temp.innerText || "";
+}
 
+function onload() {
     document.getElementById("result").style.display = 'block';
     document.getElementById("qpl").style.display = 'none';
     document.getElementById("msl").style.display = 'none';
-    
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/subjects',true);
@@ -47,24 +54,22 @@ function onload() {
         var subj = jsonResponse.Subjects.split(",")
         subj.forEach(addcat)
      };
+
      xhr.send()
-    
-
-
 }
 
-function addcat(item,index) {
+function addcat(item, index) {
     var s = document.getElementById("subjects");
-    var opt = document.createElement('option')
+    var opt = document.createElement('option');
+
     opt.value = item;
     opt.innerHTML = item;
+
     if (s.childElementCount > index) {
+        if (s.childElementCount[index].innerHTML == value) {
+            return;
+        }
+    }
 
-    if (s.childElementCount[index].innerHTML == value) {
-        return;
-    }
-    }
     s.appendChild(opt);
-
-
 }
